8117f76deb7014d1d840e406ba046f2378db2b01
Saurabh Sen <sensaurabh1212@gmail.com>
2024-02-22 14:52:17 +0530

    DEVS: electron with CRA

--- apps/electron/src/main.tsx
+++ apps/electron/src/main.tsx
@@ -1,44 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Weather from './components/Weather/Weather.tsx';
-import Todo from './components/Todo/Todo.tsx';
-import { store } from '@repo/store'
-import { Provider } from 'react-redux'
-import Home from './components/Home/index.tsx';
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+// import Weather from './components/Weather/Weather.tsx';
+// import Todo from './components/Todo/Todo.tsx';
+// import { store } from '@repo/store'
+// import { Provider } from 'react-redux'
+// import Home from './components/Home/index.tsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <App />
-    ),
-    children: [
-      {
-        path: '/',
-        element: <Home />
-      },
-      {
-        path: "weather",
-        element: <Weather />,
-      },
-      {
-        path: "todo",
-        element: <Todo />,
-      },
-    ]
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: (
+//       <App />
+//     ),
+//     children: [
+//       {
+//         path: '/',
+//         element: <Home />
+//       },
+//       {
+//         path: "weather",
+//         element: <Weather />,
+//       },
+//       {
+//         path: "todo",
+//         element: <Todo />,
+//       },
+//     ]
+//   },
+// ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <RouterProvider router={router} />
+//     </Provider>
+//   </React.StrictMode>,
+// )
